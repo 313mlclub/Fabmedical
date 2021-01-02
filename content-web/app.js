afff9ca8c5a16e3038aeb7a174dbfd92c7cf0990
313mlclub <jhwoo@microsoft.com>
2021-01-02 17:17:14 +0900

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("2ca97d67-1c2c-4177-8cff-a70ff934ca99");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://52.231.53.146:3001";
 
